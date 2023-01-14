5a83ca711f931a8ff2170dc7c1723d1674fd60e2
Gabrielle Novaes <66539847+Gaby1225@users.noreply.github.com>
2023-01-13 23:12:13 -0300

    Add files via upload

---
+++ FlightConnections.Data/FlightRoutesToData/FlightRoutesData.cs
@@ -0,0 +1,58 @@
+﻿using FlightConnections.Data.Context;
+using FlightConnections.Domain.Interfaces;
+using FlightConnections.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlightConnections.Data.FlightRoutesToData
+{
+    public class FlightRoutesData : IGenerateMethodsCrud<FlightRoutes>
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public FlightRoutesData(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<FlightRoutes> Create(FlightRoutes flight) //testar para cadastrar vários de uma vez
+        {
+            flight.Id = 0;
+            flight.RouteStart = "";
+            flight.RouteFinish = "";
+            flight.Value = 0;
+
+            _appDbContext.FlightRoute.Add(flight);
+            await _appDbContext.SaveChangesAsync();
+
+            return flight;
+        }
+
+        public async Task<IEnumerable<FlightRoutes>> Get()
+        {
+            return await _appDbContext.FlightRoute.ToListAsync();
+        }
+
+        public async Task<FlightRoutes> Get(int id)
+        {
+            return await _appDbContext.FlightRoute.FindAsync(id);
+        }
+
+        public async Task<FlightRoutes> Update(FlightRoutes flight)
+        {
+            _appDbContext.FlightRoute.Update(flight);
+            await _appDbContext.SaveChangesAsync();
+            return flight;
+        }
+
+
+        public async Task<FlightRoutes> Delete(int id)
+        {
+            var cat = await _appDbContext.FlightRoute.FindAsync(id);
+            _appDbContext.FlightRoute.Remove(cat);
+            await _appDbContext.SaveChangesAsync();
+            return cat;
+        }
+
+
+    }
+}
